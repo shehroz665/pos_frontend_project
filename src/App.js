e4a6b50c070a06abd82819a0d5270fef128e9db8
shehroz665 <shehrozmalik665@gmail.com>
2023-09-13 00:47:33 +0100

    sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import Sidebar from "./componets/Sidebar";
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Make sure to import BrowserRouter, Routes, and Route
+
+import Overview from './pages/Overview';
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
+import Team from './pages/Team';
 
 function App() {
   return (
-    <>
-      <Sidebar/>
-    </>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path='/overview' element={<Overview />} />
+        <Route path='/reports' element={<Reports />} />
+        <Route path='/reports/reports1' element={<ReportsOne />} />
+        <Route path='/reports/reports2' element={<ReportsTwo />} />
+        <Route path='/reports/reports3' element={<ReportsThree />} />
+        <Route path='/team' element={<Team />} />
+      </Routes>
+    </Router>
   );
 }
 
