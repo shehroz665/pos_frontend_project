0dccb304e8d452150c2494a62808e8e3a6ecd3a7
shehroz665 <shehrozmalik665@gmail.com>
2023-09-15 02:04:06 +0100

    sidebar removed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 import { SidebarData } from './SidebarData';
 import SubMenu from './SubMenu';
 import { IconContext } from 'react-icons/lib';
@@ -20,22 +17,13 @@ const SidebarNav = styled.nav`
   z-index: 10;
 `;
 const Sidebar = () => {
-  const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='nav'>
-          <Link to='#' className='nav-icon'>
-            <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
-        </div>
-        <SidebarNav sidebar={sidebar}>
+        <SidebarNav sidebar={true}>
           <div className='sidebar-wrap'>
-            <Link to='#' className='nav-icon'>
-              <AiIcons.AiOutlineClose onClick={showSidebar} />
-            </Link>
             {SidebarData.map((item, index) => {
               return <SubMenu item={item} key={index} />;
             })}
