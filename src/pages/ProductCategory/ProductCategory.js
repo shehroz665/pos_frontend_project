2501732c06bf860c07733a36c253be09ab490880
shehroz665 <shehrozmalik665@gmail.com>
2023-09-17 22:37:34 +0100

    change status

--- src/pages/ProductCategory/ProductCategory.js
+++ src/pages/ProductCategory/ProductCategory.js
@@ -6,11 +6,13 @@ import * as MdIcons from 'react-icons/md';
 import * as LiaIcons from 'react-icons/lia';
 import { showErrorAlert,showSuccessAlert } from '../Alerts/Alert';
 import { useNavigate } from 'react-router-dom';
+import Switch from 'react-switch';
 
 const ProductCategory = () => {
   const navigate = useNavigate();
   const [category, setCategory] = useState([]);
-  const [recallApi, setrecallApi] = useState(false)
+  const [recallApi, setrecallApi] = useState(false);
+  const [categorySwitches, setCategorySwitches] = useState(category.map(() => true));
   const [token, settoken] = useState(localStorage.getItem('token'));
   useEffect(() => {
     const apiUrl = 'http://127.0.0.1:8000/api/productcategory';
@@ -54,7 +56,25 @@ const ProductCategory = () => {
     };
     navigate('/productcategory/update', { state: propsToPass });
   }
-  
+  const handleSwitchToggle = async(id) => {
+    console.log('index',id,'token',token)
+    const apiUrl = `http://127.0.0.1:8000/api/productcategory/changeStatus/${id}`;
+    const configs = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const response = await axios.post(apiUrl, [],configs);
+      console.log('Product category deleted:', response.data);
+      showSuccessAlert('Product category status updated successfully');
+      setrecallApi((prev) => !prev);
+    } catch (error) {
+      console.error('Error updating product category status:', error);
+      showErrorAlert(error.message);
+    }
+  };
   
   return (
     <>
@@ -82,7 +102,19 @@ const ProductCategory = () => {
                 <td className={`centered ${cat.status === 1 ? 'status-active' : 'status-deactivated'}`}>
                   {cat.status === 1 ? "Active" : "Deactivated"}
                 </td>
-                <td className='centered'><><LiaIcons.LiaEdit onClick={()=> edit(cat.cat_id)} size={24} color='black'/><MdIcons.MdDelete size={24} color='rgb(206, 32, 32)' onClick={()=>handleDelete(cat.cat_id)}/></></td>
+                <td className='centered'>
+                  <>
+                  <LiaIcons.LiaEdit onClick={()=> edit(cat.cat_id)} size={24} color='black'/>
+                  <MdIcons.MdDelete size={24} color='rgb(206, 32, 32)' onClick={()=>handleDelete(cat.cat_id)}/>
+                  <Switch
+                    width={42}
+                    height={20}
+                    onChange={() => handleSwitchToggle(cat.cat_id)}
+                    checked={cat.status==1 ? true : false}
+                    offColor="#CE2020"
+                    onColor="#008000"
+                  />
+                  </></td>
               </tr>
             )))   } 
           </tbody>
