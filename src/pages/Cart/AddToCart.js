c11a2833c2e151843e1f05230ee0ca9b1bfd3970
shehroz665 <shehrozmalik665@gmail.com>
2023-10-01 22:34:09 +0100

    cart working

--- src/pages/Cart/AddToCart.js
+++ src/pages/Cart/AddToCart.js
@@ -1,15 +1,47 @@
-import React,{useState,useEffect} from 'react'
-import "../ProductCategory/AddProductCategory.css"
+import React, { useState, useEffect } from 'react';
+import "../ProductCategory/AddProductCategory.css";
 import axios from 'axios';
 import Banner from '../Banner';
 import * as AiIcons from 'react-icons/ai';
+import { showErrorAlert, showSuccessAlert } from '../Alerts/Alert';
 const AddToCart = () => {
   const [availableItems, setAvailableItems] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
   const token = localStorage.getItem('token');
+
   const addToCart = (item) => {
-    setSelectedItems([...selectedItems, item]);
+    const isAlreadySelected = selectedItems.some((selectedItem) => selectedItem.prod_id === item.prod_id);
+    if (!isAlreadySelected) {
+      setSelectedItems([...selectedItems, { ...item, quantity: 1 }]);
+    } else {
+      showErrorAlert("Item is already in the cart");
+    }
   };
+
+  const incrementQuantity = (item) => {
+    const updatedSelectedItems = selectedItems.map((selectedItem) => {
+      if (selectedItem.prod_id === item.prod_id) {
+        if (selectedItem.quantity < parseFloat(item.prod_quantity)) {
+          return { ...selectedItem, quantity: selectedItem.quantity + 1 };
+        } else {
+          showErrorAlert(`Cannot increase quantity beyond the available stock (${parseInt(item.prod_quantity)})`);
+        }
+      }
+      return selectedItem;
+    });
+    setSelectedItems(updatedSelectedItems);
+  };
+  
+  const decrementQuantity = (item) => {
+    const updatedSelectedItems = selectedItems.map((selectedItem) => {
+      if (selectedItem.prod_id === item.prod_id && selectedItem.quantity > 1) {
+        return { ...selectedItem, quantity: selectedItem.quantity - 1 };
+      }
+      return selectedItem;
+    });
+    setSelectedItems(updatedSelectedItems);
+  };
+
   useEffect(() => {
     const apiUrl = `http://127.0.0.1:8000/api/product`;
     const config = {
@@ -18,50 +50,70 @@ const AddToCart = () => {
         'Content-Type': 'application/json',
       },
     };
-    axios.get(apiUrl,config)
+    axios.get(apiUrl, config)
       .then((response) => {
         console.log(response.data.data.data)
-      
         setAvailableItems(response.data.data.data);
-
       })
       .catch((error) => {
         console.error('Error fetching product data:', error);
       });
   }, [token]);
-  return (
-   <>
-  <Banner title={"View Products"}/>
-   <div className='home'>
-   <div className="add-to-cart-container">
-      <div className="available-items">
-        <h2>Available Items</h2>
-        <div className="scrollable-items">
-          {availableItems.map((item) => (
-            <div key={item.prod_id} onClick={() => addToCart(item)}>
-              {item.prod_name}
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="selected-items">
-        <h2>Selected Items</h2>
-        <div>
-          {selectedItems.map((item) => (
-            <div key={item.prod_id}>{item.prod_name}
-            <>
-            <AiIcons.AiOutlinePlus/>
-            <AiIcons.AiOutlineMinus/>
-            </>
-            </div>
 
-          ))}
+  return (
+    <>
+      <Banner title={"Add to Cart"} />
+      <div className='home'>
+        <div className="add-to-cart-container">
+          <div className="available-items">
+            <h2>Products</h2>
+            <div className="scrollable-items">
+            <table>
+              <thead>
+                <tr>
+                  <th>Sr.</th>
+                  <th>Product Name</th>
+                  <th>Supplier</th>
+                  <th>Category</th>
+                  <th>Quantity</th>                    
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {availableItems.map((item) => (
+                  <tr key={item.prod_id}>
+                    <td>{item.prod_id}</td>
+                    <td>{item.prod_name}</td>
+                    <td>{item.sup_name}</td>
+                    <td>{item.cat_name}</td>
+                    <td>{parseInt(item.prod_quantity)}</td>
+                    <td>
+                      <button onClick={() => addToCart(item)}>Add</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          </div>
+          <div className="selected-items">
+            <h2>Cart</h2>
+            <div className="scrollable-items">
+              {selectedItems.map((item) => (
+                <div key={item.prod_id}>
+                  {item.prod_name}
+                  <AiIcons.AiOutlinePlus onClick={() => incrementQuantity(item)}/>
+                  <span>{item.quantity}</span>
+                  <AiIcons.AiOutlineMinus onClick={() => decrementQuantity(item)}/>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-   </div>
-   </>
+    </>
   )
 }
 
-export default AddToCart
+export default AddToCart;
