5896f989d219261c0a55ae17ee58e5573ce2f9de
shehroz665 <shehrozmalik665@gmail.com>
2023-10-10 02:55:40 +0500

    no record found added in sales tables

--- src/pages/Sale/Sales.js
+++ src/pages/Sale/Sales.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "../ProductCategory/ProductCategory.css"
 import Banner from '../Banner';
 import axios from 'axios';
@@ -16,9 +16,9 @@ const Sales = () => {
   const [date, setDate] = useState(formatDate(new Date()));
   const [salesCalculations, setsalesCalculations] = useState({});
   useEffect(() => {
-    console.log('input date',date);
-    const formatedDate =formatDateToDDMMYYYY(date);
-    console.log('formatted Date',formatedDate)
+    console.log('input date', date);
+    const formatedDate = formatDateToDDMMYYYY(date);
+    console.log('formatted Date', formatedDate)
     const apiUrl = `http://127.0.0.1:8000/api/invoice?page=${currentPage}&per_page=${itemsPerPage}&search=${formatedDate}`;
     const config = {
       headers: {
@@ -26,7 +26,7 @@ const Sales = () => {
         'Content-Type': 'application/json',
       },
     };
-    axios.get(apiUrl,config)
+    axios.get(apiUrl, config)
       .then((response) => {
         setinvoice(response.data.data.data);
         settotalPages(response.data.data.last_page);
@@ -34,10 +34,10 @@ const Sales = () => {
       .catch((error) => {
         console.error('Error fetching product data:', error);
       });
-  }, [currentPage,token,date]);
+  }, [currentPage, token, date]);
   useEffect(() => {
-    console.log('date',formatDateToDDMMYYYY(date));
-    const formatedDate =formatDateToDDMMYYYY(date);
+    console.log('date', formatDateToDDMMYYYY(date));
+    const formatedDate = formatDateToDDMMYYYY(date);
     const apiUrl = `http://127.0.0.1:8000/api/invoice/sales?&search=${formatedDate}`;
     const config = {
       headers: {
@@ -45,24 +45,24 @@ const Sales = () => {
         'Content-Type': 'application/json',
       },
     };
-    axios.get(apiUrl,config)
+    axios.get(apiUrl, config)
       .then((response) => {
-       console.log(response.data.data);
-       setsalesCalculations(response.data.data);
+        console.log(response.data.data);
+        setsalesCalculations(response.data.data);
       })
       .catch((error) => {
         console.error('Error fetching product data:', error);
       });
-  }, [token,date]);
+  }, [token, date]);
   function formatDate(inputDate) {
-  const date = new Date(inputDate);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+    const date = new Date(inputDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
-  const edit=(id)=> {
-    console.log('edit',id);
+  const edit = (id) => {
+    console.log('edit', id);
     const propsToPass = {
       id: id,
     };
@@ -74,92 +74,102 @@ const Sales = () => {
   function formatDateToDDMMYYYY(inputDate) {
     const parts = inputDate.split('-');
     if (parts.length === 3) {
-        const [year, month, day] = parts;
-        const shortenedYear = year.slice(-2);
-        return `${day}-${month}-${shortenedYear}`;
+      const [year, month, day] = parts;
+      const shortenedYear = year.slice(-2);
+      return `${day}-${month}-${shortenedYear}`;
     }
   }
   return (
     <>
-    <Banner title={"View Sales"}/>
-    <div className='home'>
-    <div className='search-container'>
-            <input
-              type='date'
-              className='search-input'
-              value={date}
-              onChange={(e)=>setDate(e.target.value)}
-            />
-      </div>
+      <Banner title={"View Sales"} />
+      <div className='home'>
+        <div className='search-container'>
+          <input
+            type='date'
+            className='search-input'
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
         <div className='table-container'>
-        <table>
-          <thead>
-            <tr>
-              <th>Total Products</th>
-              <th className='centered'>Total Quantity</th>
-              <th className='centered'>Total Price</th>
-              <th className='centered'>Total Cost</th> 
-              <th className='centered'>Total Profit</th>
-            </tr>
-          </thead>
-          <tbody>
+          <table>
+            <thead>
               <tr>
-                <td>{salesCalculations.total_products_sum}</td>
-                <td className='centered'>{salesCalculations.total_quantity_sum}</td>
-                <td className='centered'>{salesCalculations.total_price_sum}</td>
-                <td className='centered'>{salesCalculations.total_cost_sum}</td>
-                <td className='centered'>{isNaN(parseInt(salesCalculations.total_price_sum)
-                -parseInt(salesCalculations.total_cost_sum))? 0 : parseInt(salesCalculations.total_price_sum)
-                -parseInt(salesCalculations.total_cost_sum)}</td>
+                <th>Total Products</th>
+                <th className='centered'>Total Quantity</th>
+                <th className='centered'>Total Price</th>
+                <th className='centered'>Total Cost</th>
+                <th className='centered'>Total Profit</th>
               </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className='table-container'>
-        <table>
-          <thead>
-            <tr>
-              <th>Sr.</th>
-              <th className='centered'>Customer Name</th>
-              <th className='centered'>Contact</th>
-              <th className='centered'>Total Products</th> 
-              <th className='centered'>Total Price</th> 
-              <th className='centered'>Total Cost</th> 
-              <th className='centered'>Total Quantity</th>
-              <th className='centered'>Created Date</th>
-              <th className='centered'>Updated Date</th>                     
-              <th className='centered'>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-           {invoice.length===0? <tr>
-      <td colSpan="10" className="centered">
-        No records found
-      </td>
-    </tr>  :(invoice.map((inv) => (
-              <tr key={inv.invoice_id}>
-                <td>{inv.invoice_id}</td>
-                <td className='centered'>{inv.cust_name}</td>
-                <td className='centered'>{inv.cust_number}</td>
-                <td className='centered'>{inv.total_products}</td>
-                <td className='centered'>Rs {inv.total_price}</td>
-                <td className='centered'>Rs {inv.total_cost}</td>
-                <td className='centered'>{inv.total_quantity}</td>
-                <td className='centered'>{inv.created_date}</td>
-                <td className='centered'>{inv.updated_date}</td>
-                <td className='centered'>
-                  <TbIcons.TbFileInvoice onClick={()=> edit(inv.invoice_id)} size={24} color='black'/></td>
+            </thead>
+            <tbody>
+              {parseInt(salesCalculations.total_products_sum) === 0 ? (
+                <tr>
+                  <td colSpan="5" className="centered">
+                    No records found
+                  </td>
+                </tr>
+              ) : (
+                <tr>
+                  <td>{salesCalculations.total_products_sum}</td>
+                  <td className='centered'>{salesCalculations.total_quantity_sum}</td>
+                  <td className='centered'>{salesCalculations.total_price_sum}</td>
+                  <td className='centered'>{salesCalculations.total_cost_sum}</td>
+                  <td className='centered'>
+                    {isNaN(parseInt(salesCalculations.total_price_sum) - parseInt(salesCalculations.total_cost_sum)) ? 0 :
+                      parseInt(salesCalculations.total_price_sum) - parseInt(salesCalculations.total_cost_sum)}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+
+        </div>
+        <div className='table-container'>
+          <table>
+            <thead>
+              <tr>
+                <th>Sr.</th>
+                <th className='centered'>Customer Name</th>
+                <th className='centered'>Contact</th>
+                <th className='centered'>Total Products</th>
+                <th className='centered'>Total Price</th>
+                <th className='centered'>Total Cost</th>
+                <th className='centered'>Total Quantity</th>
+                <th className='centered'>Created Date</th>
+                <th className='centered'>Updated Date</th>
+                <th className='centered'>Actions</th>
               </tr>
-            )))   } 
-          </tbody>
-        </table>
-      </div>
-      <ResponsivePagination
+            </thead>
+            <tbody>
+              {invoice.length === 0 ? <tr>
+                <td colSpan="10" className="centered">
+                  No records found
+                </td>
+              </tr> : (invoice.map((inv) => (
+                <tr key={inv.invoice_id}>
+                  <td>{inv.invoice_id}</td>
+                  <td className='centered'>{inv.cust_name}</td>
+                  <td className='centered'>{inv.cust_number}</td>
+                  <td className='centered'>{inv.total_products}</td>
+                  <td className='centered'>Rs {inv.total_price}</td>
+                  <td className='centered'>Rs {inv.total_cost}</td>
+                  <td className='centered'>{inv.total_quantity}</td>
+                  <td className='centered'>{inv.created_date}</td>
+                  <td className='centered'>{inv.updated_date}</td>
+                  <td className='centered'>
+                    <TbIcons.TbFileInvoice onClick={() => edit(inv.invoice_id)} size={24} color='black' /></td>
+                </tr>
+              )))}
+            </tbody>
+          </table>
+        </div>
+        <ResponsivePagination
           current={currentPage}
           total={totalPages}
           onPageChange={handlePageChange}
         />
-    </div>
+      </div>
     </>
   )
 }
