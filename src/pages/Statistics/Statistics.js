9bdd81d3e991abd67ee4f7c404a2bc0bfc3f6c1a
shehroz665 <shehrozmalik665@gmail.com>
2023-10-10 04:46:21 +0500

    getWeeklyStatistics api handled in statistics

--- src/pages/Statistics/Statistics.js
+++ src/pages/Statistics/Statistics.js
@@ -1,5 +1,5 @@
-import React,{useState,useEffect} from 'react';
-import { useNavigate,Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import "../ProductCategory/ProductCategory.css";
 import axios from 'axios';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
@@ -12,11 +12,17 @@ const Statistics = () => {
   const [totoalSalesToday, settotoalSalesToday] = useState(0);
   const [totalStock, settotalStock] = useState(0);
   const [totalCategory, settotalCategory] = useState(0);
+  const [graphdata, setgraphdata] = useState([
+    { name: '', sales: 0, },
+    { name: '', sales: 0, },
+    { name: '', sales: 0, },
+    { name: '', sales: 0, },
+    { name: '', sales: 0, },]);
   const data = [
-    { name: 'Category A', sales: 4000,  },
-    { name: 'Category B', sales: 3000,  },
-    { name: 'Category C', sales: 2000,  },
-    { name: 'Category D', sales: 2780,  },
+    { name: 'Category A', sales: 4000, },
+    { name: 'Category B', sales: 3000, },
+    { name: 'Category C', sales: 2000, },
+    { name: 'Category D', sales: 2780, },
     { name: 'Category E', sales: 1890, },
   ];
   useEffect(() => {
@@ -27,25 +33,26 @@ const Statistics = () => {
         'Content-Type': 'application/json',
       },
     };
-    axios.get(apiUrl,config)
+    axios.get(apiUrl, config)
       .then((response) => {
         console.log(response.data.data);
-        const responseData =response.data.data;
+        const responseData = response.data.data;
         settotoalSalesToday(responseData.sales.total_price_sum);
         settotalStock(parseInt(responseData.totalStock));
         settotalProducts(responseData.products);
         settotalCategory(responseData.categories);
         settotalSuppliers(responseData.suppliers);
- 
+        setgraphdata(responseData.graphStatistics);
+
       })
       .catch((error) => {
         console.error('Error fetching product data:', error);
       });
-  }, [recallApi,token]);
+  }, [recallApi, token]);
   return (
     <div className='home'>
       <div className='statistics-container'>
-      <Link to="/sales" className="statistic-box" onClick={()=> navigate('/sales')}>
+        <Link to="/sales" className="statistic-box" onClick={() => navigate('/sales')}>
           <h2>Today Sales</h2>
           <p>Rs {totoalSalesToday}</p>
         </Link>
@@ -53,23 +60,23 @@ const Statistics = () => {
           <h2>Total Stock</h2>
           <p>Rs {totalStock}</p>
         </div>
-        <Link to="/products" className="statistic-box" onClick={()=> navigate('/products')}>
+        <Link to="/products" className="statistic-box" onClick={() => navigate('/products')}>
           <h2>Total Products</h2>
           <p>{totalProducts}</p>
         </Link>
-        <Link to="/suppliers" className="statistic-box" onClick={()=> navigate('/suppliers')}>
+        <Link to="/suppliers" className="statistic-box" onClick={() => navigate('/suppliers')}>
           <h2>Total Suppliers</h2>
           <p>{totalSuppliers}</p>
         </Link>
-        <Link to="/productcategory" className="statistic-box" onClick={()=> navigate('/productcategory')}>
+        <Link to="/productcategory" className="statistic-box" onClick={() => navigate('/productcategory')}>
           <h2>Total Categories</h2>
           <p>{totalCategory}</p>
         </Link>
       </div>
       <div className='statistics-container'>
-      <div className="statistics-graph-container">
-        <h1>graph</h1>
-        <BarChart width={600} height={400} data={data}>
+        <div className="statistics-graph-container">
+          <h1>graph</h1>
+          <BarChart width={600} height={400} data={graphdata}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
@@ -77,10 +84,10 @@ const Statistics = () => {
             <Legend />
             <Bar dataKey="sales" fill="#8884d8" />
           </BarChart>
-      </div>
-      <div className="statistics-graph-container">
-        <h1>others</h1>
-      </div>
+        </div>
+        <div className="statistics-graph-container">
+          <h1>others</h1>
+        </div>
       </div>
     </div>
   );
